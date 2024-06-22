2d9a54c3838d8e2553863168879cfdbc97b3e873
NachoR9 <nachorm9@hotmail.com>
2024-06-21 19:38:33 -0700

    explanation ex-4

--- ex-4/ex-4.js
+++ ex-4/ex-4.js
@@ -1,6 +1,9 @@
 // Escribe un programa que pida dos números y escriba “La suma de <numero-uno> 
 // con <numero-dos> es <resultado>”.
 
+// Añadir un event listener de forma que cada vez que el botón se pulse coja los valores
+// que hay en los inputs los convierta a valores numéricos y pinte la suma
+
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2")
 const button = document.getElementById("btn")
