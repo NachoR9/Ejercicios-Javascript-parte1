3d99f9db8ec83a565dd3edb8968be043a844a81f
NachoR9 <nachorm9@hotmail.com>
2024-06-23 16:10:00 -0700

    ex-15 done

--- ex-15/ex-15.js
+++ ex-15/ex-15.js
@@ -1,4 +1,4 @@
-//Escribir un programa que escriba en pantalla los divisores de un número dado
+//Escribir un programa que nos diga si un número dado es primo (no es divisible por ninguno otro número que no sea él mismo o la unidad)
  
 //Definimos una variable que almacene si el número es primo y establecemos que por defecto sí.
 // Recorremos todos los números menores que el valor dado y si encontramos uno por el que sea divisible, 
