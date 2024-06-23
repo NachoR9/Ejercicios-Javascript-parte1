c8550e47f3e70737f6cb0e1d2454803c8edd6ef3
NachoR9 <nachorm9@hotmail.com>
2024-06-23 16:08:36 -0700

    ex-15 done

---
+++ ex-15/ex-15.js
@@ -0,0 +1,27 @@
+//Escribir un programa que escriba en pantalla los divisores de un número dado
+ 
+//Definimos una variable que almacene si el número es primo y establecemos que por defecto sí.
+// Recorremos todos los números menores que el valor dado y si encontramos uno por el que sea divisible, 
+// asignamos la variable a false, paramos y pintamos el resultado de si es primo o no.
+
+const number = document.getElementById("number");
+const button = document.getElementById("btn")
+const response = document.getElementById("p-id");
+
+button.addEventListener("click", (e) => {
+  const value = parseInt(number.value);
+  let isPrime = true;
+ 
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+        isPrime = false;
+        break
+    }
+  }
+
+  if (isPrime) {
+    response.textContent = `El número ${value} es primo`
+  } else {
+    response.textContent = `El número ${value} no es primo`
+  }
+});
